ca422bdcec1be7121e6c44a6525b06ea8a382b70
Logan Bertrand <loganwbertrand@gmail.com>
2019-07-05 12:52:26 -0700

    initial functionality working along with comments

--- public/app.js
+++ public/app.js
@@ -2,57 +2,54 @@
 $.getJSON("/articles", function(data) {
     // For each one
     for (var i = 0; i < data.length; i++) {
-      // Display the apropos information on the page
-      $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+      // Display the information on the page. Apologies for how unclean this looks
+      $("#articles").append("<div class='card'> <div class='card-body'> <p id='ptag"+ data[i]._id +"' data-id='" + data[i]._id + "'class='h4'>" + data[i].title + "<br /><a href=" + data[i].link + ">Link to Article</a></p> <p id='comment"+ data[i]._id +"' data-id='" + data[i]._id + "'class='h4'>Comments: <br> </p> <button type='button' data-id='" + data[i]._id + "' class='btn btn-info article-btn'>Comment</button></div></div>");
     }
   });
   
   
   // Whenever someone clicks a p tag
-  $(document).on("click", "p", function() {
+  $(document).on("click", ".article-btn", function() {
     // Save the id from the p tag
     var thisId = $(this).attr("data-id");
+    var pTag = $("#ptag"+thisId+"")
   
     // Now make an ajax call for the Article
     $.ajax({
       method: "GET",
       url: "/articles/" + thisId
     })
-      // With that done, add the note information to the page
+      // With that done, add the comment information to the page
       .then(function(data) {
         console.log(data);
         // The title of the article
-        $("#comments").append("<h2>" + data.title + "</h2>");
+        $(pTag).append("<h2>" + data.title + "</h2>");
         // An input to enter a new title
-        $("#comments").append("<input id='titleinput' name='title' >");
-        // A textarea to add a new note body
-        $("#comments").append("<textarea id='bodyinput' name='body'></textarea>");
-        // A button to submit a new note, with the id of the article saved to it
-        $("#comments").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+        $(pTag).append("<input id='titleinput' name='title' ><br>");
+        // A textarea to add a new comment body
+        $(pTag).append("<textarea id='bodyinput' name='body'></textarea>");
+        // A button to submit a new comment, with the id of the article saved to it
+        $(pTag).append("<button data-id='" + data._id + "' id='savecomment'>Save Comment</button>");
   
-        // If there's a note in the article
-        if (data.note) {
-          // Place the title of the note in the title input
-          $("#titleinput").val(data.note.title);
-          // Place the body of the note in the body textarea
-          $("#bodyinput").val(data.note.body);
-        }
+
       });
   });
   
-  // When you click the savenote button
-  $(document).on("click", "#savenote", function() {
+  // When you click the savecomment button
+  $(document).on("click", "#savecomment", function() {
     // Grab the id associated with the article from the submit button
     var thisId = $(this).attr("data-id");
-  
-    // Run a POST request to change the note, using what's entered in the inputs
+    var commentTag = $("#comment"+thisId+"")
+    var titleInput = $('#titleinput').val();
+    var commentInput = $('#bodyinput').val();
+    // Run a POST request to change the comment, using what's entered in the inputs
     $.ajax({
       method: "POST",
       url: "/articles/" + thisId,
       data: {
         // Value taken from title input
         title: $("#titleinput").val(),
-        // Value taken from note textarea
+        // Value taken from comment textarea
         body: $("#bodyinput").val()
       }
     })
@@ -60,11 +57,11 @@ $.getJSON("/articles", function(data) {
       .then(function(data) {
         // Log the response
         console.log(data);
-        // Empty the comments section
-        $("#comments").empty();
+        $(commentTag).append("<br>"+ titleInput + "<br>")
+        $(commentTag).append("<br>"+ commentInput + "<br>")
       });
   
-    // Also, remove the values entered in the input and textarea for note entry
+    // Also, remove the values entered in the input and textarea for comment entry
     $("#titleinput").val("");
     $("#bodyinput").val("");
   });
